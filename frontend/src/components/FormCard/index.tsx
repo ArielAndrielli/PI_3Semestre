723e94efc049f1705803ac4c3e1dae722a19fb28
Vitor Henrique <vitorsantos.info@gmail.com>
2022-05-15 21:02:15 -0300

    Ajustes

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -25,7 +25,7 @@ function FormCard({movieId} : Props) {
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
             <div className="dsmovie-card-bottom-container">
-                <h3>"The Witcher"</h3>
+                <h3>{movie?.title}</h3>
                 <form className="dsmovie-form">
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="email">Informe seu email</label>
