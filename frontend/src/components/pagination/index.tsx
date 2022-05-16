12067a2eb85525fae54b36b8f86b4bc71b034753
Vitor Henrique <vitorsantos.info@gmail.com>
2022-05-15 22:41:58 -0300

    Ajustes

--- frontend/src/components/pagination/index.tsx
+++ frontend/src/components/pagination/index.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as Arrow } from 'assets/img/arrow.svg';
 import { MoviePage } from 'types/movie';
-import './styles.css';
+import './style.css';
 
 type Props = {
     page: MoviePage;
